39fca7353a0e94adc2c14ea7d08c928037723f60
Hendrik <Asmod4n@users.noreply.github.com>
2026-09-12 14:58:28 +0200

    Delete bench/vm directory

--- bench/vm/bench_counter.rb
+++
@@ -1,9 +0,0 @@
-class BenchCounter < Webmachine::Resource
-  def initialize
-    @n = 0
-  end
-
-  def to_html
-    "<html><body>hit #{@n += 1}</body></html>"
-  end
-end
--- bench/vm/handlers.rb
+++
@@ -1,6 +0,0 @@
-
-def wm_noop; end
-
-def wm_handle(req)
-  "HTTP/1.1 200 OK\r\nContent-Length: 2\r\n\r\nOK"
-end
